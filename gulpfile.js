a78f0009ab0f3ab74bf805351d4e8811bbf1939c
Darima Ganzhurova <DGanzhurova@gmail.com>
2020-06-30 18:06:08 +0900

    Новая сборка, исправленно лого в index

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,28 @@
-"use strict";
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const browserSync = require("browser-sync").create();
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
+gulp.task("css", () => {
+  return gulp
+    .src(["source/sass/style.scss"])
     .pipe(plumber())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer]))
     .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(browserSync.stream());
 });
 
-gulp.task("server", function () {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
+gulp.task("serve", () => {
+  browserSync.init({
+    server: "./source",
+    notify: false
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html").on("change", browserSync.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("start", gulp.series("css", "serve"));
